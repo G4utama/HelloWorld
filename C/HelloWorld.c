709d030973254ca0f39e82d3ca28c97cf3ae5aac
Leonardo <thinkpad@thinkpad.wind3.hub>
2026-01-24 16:54:49 +0100

    Hello, World! in C

---
+++ C/HelloWorld.c
@@ -0,0 +1,11 @@
+// Hello, World! in C
+
+#include <stdio.h>
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
+
+// input: gcc HelloWorld.c -o HelloWorld.out
+// input: ./HelloWorld.out
+// output: Hello, World!
