b6893442b335c2f8a5db63611744219413f70ff8
Leonardo <thinkpad@thinkpad.wind3.hub>
2026-01-25 16:18:56 +0100

    Hello, World! in C++

---
+++ C++/HelloWorld.cpp
@@ -0,0 +1,12 @@
+// Hello, World! in C++
+
+#include <iostream>
+
+int main() {
+    std::cout<<"Hello, World!"<<std::endl;
+    return 0;
+}
+
+// input: g++ HelloWorld.cpp -o HelloWorld.out
+// input: ./HelloWorld.out
+// output: Hello, World!
